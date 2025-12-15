f2352e67ef580dfbf2eeeebdbe00e093a62ced1a
GixCoder999 <msohaib0503@gmail.com>
2025-12-15 22:21:12 +0500

    testing

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -11,5 +11,5 @@ const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,  // limits simultaneous connections
 });
-
+const db = pool.promise();
 module.exports = db;
