ede7f0671c945a703e9045d828afa591c2a5de02
GixCoder999 <msohaib0503@gmail.com>
2025-12-15 22:51:16 +0500

    testing

--- backend/generateLogins.js
+++ backend/generateLogins.js
@@ -5,7 +5,7 @@ module.exports = () => {
   cron.schedule('*/1 * * * *', () => { // every minute
     console.log('⏰ Running login credentials generation job...');
 
-    const sql0 = `UPDATE students set username = concat(replace(name,' ',''),id)`;
+    const sql0 = `UPDATE students set username = concat(replace(name,' ',''),id) where created_at >= NOW() - INTERVAL 1 HOUR`;
 
     db.query(sql0,(err,result)=>{
         if (err) {
